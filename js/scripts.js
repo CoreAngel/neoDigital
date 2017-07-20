cc5f4a9d01154b6b71f953540940f6919a534368
CoreAngel <bragon912@gmail.com>
2017-07-20 21:19:56 +0200

    fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,29 @@
 (function () {
 
-    var currentTabID = 1;
+    let currentTabID = 1;
 
-    var changeTabs = function(e) {
+    let changeTabs = function(e) {
         e.preventDefault();
 
         if (e.target && e.target.nodeName == "LI" || e.target.nodeName == "A") {
 
-            var elementLi =  e.target;
+            let elementLi =  e.target;
 
             if(e.target.nodeName == "A") {
                 elementLi = e.target.parentNode;
             }
 
-            var elementID = elementLi.id;
-            var elementNumber = elementID.replace("serviceNavTab-", "");
-            var tabID = "serviceTab-" + elementNumber;
-            var magicRec = document.querySelector("#magicRec");
-            var offsetList = document.querySelector("#serviceNavTabs__list").offsetLeft;
-            var offsetListElement = document.querySelector("#"+elementID).offsetLeft;
+            let elementID = elementLi.id;
+            let elementNumber = elementID.replace("serviceNavTab-", "");
+            let tabID = "serviceTab-" + elementNumber;
+            let magicRec = document.querySelector("#magicRec");
+            let offsetList = document.querySelector("#serviceNavTabs__list").offsetLeft;
+            let offsetListElement = document.querySelector("#"+elementID).offsetLeft;
 
             magicRec.style.width = elementLi.offsetWidth + "px";
             magicRec.style.left = offsetListElement - offsetList + "px";
 
-            if(currentTabID != elementNumber) {
+            if(currentTabID !== elementNumber) {
                 document.querySelector("#" + tabID).classList.add("serviceTabs__tab--visible");
                 document.querySelector("#" + "serviceTab-" + currentTabID).classList.remove("serviceTabs__tab--visible");
 
@@ -31,38 +31,37 @@
             }
 
         }
-    }
+    };
 
 
-    var letVisibleTeam = function (e) {
+    let letVisibleTeam = function (e) {
         let focuedElement = e.target;
         focuedElement.parentNode.parentNode.classList.add("member__container--visible");
-    }
+    };
 
 
-    var removeVisibleTeam = function (e) {
+    let removeVisibleTeam = function (e) {
         let focuedElement = e.target;
         focuedElement.parentNode.parentNode.classList.remove("member__container--visible");
-    }
+    };
 
-    var clickCloseMobileMenu = function (e) {
-        var navBarElement = document.querySelector("#navBarContainer");
+    let clickCloseMobileMenu = function (e) {
+        let navBarElement = document.querySelector("#navBarContainer");
         if(!navBarElement.contains(e.target)) {
             navBarElement.querySelector('.navigation').classList.remove('navigation--open');
         }
-    }
+    };
 
-    var smoothScroll = function (to, time, e) {
-        var directory,
-            distance;
-
-        var startScrollTop = document.body.scrollTop
-        var tempDistance = to.offsetTop - startScrollTop;
+    let smoothScroll = function (to, time, e) {
+        let directory,
+            distance,
+            startScrollTop = document.body.scrollTop,
+            tempDistance = to.offsetTop - startScrollTop;
 
         if(tempDistance < 0) {
             directory = 'up';
             distance = tempDistance * (-1);
-        }else if(tempDistance == 0) {
+        }else if(tempDistance === 0) {
             window.location.hash = e.target.getAttribute("href");
             return;
         } else {
@@ -70,11 +69,11 @@
             distance = tempDistance;
         }
 
-        var x = 0;
-        var step = 1/time*5;
-        if(directory == 'down') {
-            var interval = setInterval(function () {
-                var number = 3 * Math.pow(x,2) - 2 * Math.pow(x,3);
+        let x = 0;
+        let step = 1/time*5;
+        if(directory === 'down') {
+            let interval = setInterval(function () {
+                let number = 3 * Math.pow(x,2) - 2 * Math.pow(x,3);
 
                 x+=step;
                 if(x >= 1) {
@@ -84,8 +83,8 @@
                 window.scrollTo(0, startScrollTop+(number*distance));
             }, 5)
         } else {
-            var interval = setInterval(function () {
-                var number = 3 * Math.pow(x,2) - 2 * Math.pow(x,3);
+            let interval = setInterval(function () {
+                let number = 3 * Math.pow(x,2) - 2 * Math.pow(x,3);
 
                 x+=step;
                 if(x >= 1) {
@@ -95,9 +94,7 @@
                 window.scrollTo(0, startScrollTop-(number*distance));
             }, 5)
         }
-
-
-    }
+    };
 
 
     document.querySelector("#serviceNavTabs__list").addEventListener('click', function(e){
@@ -111,7 +108,7 @@
         button.addEventListener('blur', function(e){
             removeVisibleTeam(e);
         });
-    })
+    });
 
     document.addEventListener('click', clickCloseMobileMenu);
 
@@ -132,4 +129,4 @@
         });
     })
 
-})()
+})();
